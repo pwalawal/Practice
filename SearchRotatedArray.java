590abc1e7f044b3391349a717a57fd32df9eb0d3
pwalawal <pwalawal@ufl.edu>
2016-08-22 03:32:49 -0400

    added  Leet Code Program to get or search a element from rotated sorted array

---
+++ SearchRotatedArray.java
@@ -0,0 +1,39 @@
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+*/
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        
+      int left = 0;
+    int right= nums.length-1;
+ 
+    while(left<=right){
+        int mid = (right+left)/2;
+        if(target==nums[mid])
+            return mid;
+ 
+        if(nums[left]<=nums[mid]){
+            if(nums[left]<=target&& target<nums[mid]){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }else{
+            if(nums[mid]<target&& target<=nums[right]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }    
+    }
+ 
+    return -1;
+    }
+}
