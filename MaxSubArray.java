d44be137a9b9e5343ca3b4cb5741a56341acaee3
pwalawal <pwalawal@ufl.edu>
2016-08-22 03:45:51 -0400

    added  Leet Code Program to find contigous sub array within an array

---
+++ MaxSubArray.java
@@ -0,0 +1,18 @@
+\\Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int max=nums[0];
+        int sum=nums[0];
+        
+        int len=nums.length;
+        for(int i=1;i<len;i++)
+        {
+            sum=Math.max(nums[i],sum+nums[i]);
+            max=Math.max(max,sum);
+        }
+        
+        return max;
+    }
+}
