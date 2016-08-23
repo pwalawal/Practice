931484fcb53764372ea2a8f7e1c0019ac5f35306
pwalawal <pwalawal@ufl.edu>
2016-08-22 20:29:30 -0400

    added  Leet Code Program to remove all elements from list that have value as val

---
+++ removeVal.java
@@ -0,0 +1,28 @@
+//Remove all elements from a linked list of integers that have value val.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode helper = new ListNode(0);
+        helper.next = head;
+        ListNode p = helper;
+ 
+        while(p.next != null){
+            if(p.next.val == val){
+                p.next=p.next.next; 
+            }else{
+                p = p.next;
+             }
+    }
+ 
+    return helper.next;
+        
+    }
+}
