5137a5962f7e9b878da62b9e02bf27fe5afd7035
pwalawal <pwalawal@ufl.edu>
2016-10-15 05:18:24 -0400

    Leet code program to find where a cycle begins in a linked list if a cycle exists

---
+++ linkedListCycle2.java
@@ -0,0 +1,48 @@
+/*
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head==null||head.next==null)
+        {
+            return null;
+        }
+        
+        ListNode slow=head;
+        ListNode fast=head.next;
+        
+        while(fast!=slow)
+        {
+            if(fast==null||fast.next==null)
+            {
+                return null;
+            }
+            
+            fast=fast.next.next;
+            slow=slow.next;
+            
+        }
+        
+        while(head!=slow.next)
+        {
+            head=head.next;
+            slow=slow.next;
+        }
+        return head;
+    }
+}
