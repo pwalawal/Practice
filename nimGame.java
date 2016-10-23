1e7ff3ac0b9c6e37df1048e03cb03dece75e314d
pwalawal <pwalawal@ufl.edu>
2016-10-23 09:09:58 -0400

    Leetcode program to determine if we can win nim game

---
+++ nimGame.java
@@ -0,0 +1,18 @@
+/*
+You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+
+Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
+*/
+
+public class Solution {
+    public boolean canWinNim(int n) {
+        
+        //The only time the first person taking first coin has no chance of winning is if the number of coins are multiple f 4
+        if(n%4==0)
+        {
+            return false;
+        }
+        return true;
+        
+    }
+}
