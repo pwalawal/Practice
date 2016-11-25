22ccd09db0e6897eb5035e830641a5ef01903614
pwalawal <pwalawal@ufl.edu>
2016-11-24 19:55:58 -0500

    leetcode program to find sum of two integer

---
+++ sum.java
@@ -0,0 +1,16 @@
+//Sum of Two Integers
+
+public class Solution {
+    public int getSum(int a, int b) {
+        
+        int c;
+        while(b!=0)
+        {
+            c=a&b;
+            a=a^b;
+            b=c<<1;
+        }
+        return a;
+        
+    }
+}
