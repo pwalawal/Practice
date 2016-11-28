4626f251dcc0b0743a8d6a49e67ab26da22d83c9
pwalawal <pwalawal@ufl.edu>
2016-11-27 20:58:33 -0500

    leetcode program to reverse string

---
+++ reverse.java
@@ -0,0 +1,10 @@
+//Reverse String
+
+public class Solution {
+    public String reverseString(String s) {
+        
+        StringBuilder sb=new StringBuilder(s);
+        return sb.reverse().toString();
+        
+    }
+}
