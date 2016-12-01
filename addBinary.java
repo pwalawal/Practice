baf7ef52bbce421dcd33b3aa719c808861d1b8bd
pwalawal <pwalawal@ufl.edu>
2016-11-30 20:36:14 -0500

    leetcode program to add Binary

---
+++ addBinary.java
@@ -0,0 +1,27 @@
+//AddBinary
+
+public class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb=new StringBuilder();
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        while(i>=0||j>=0){
+            int sum=carry;
+            if(j>=0){
+                sum=sum+b.charAt(j)-'0';
+                j--;
+            }
+            if(i>=0){
+                sum=sum+a.charAt(i)-'0';
+                i--;
+            }
+            sb.append(sum%2);
+            carry=sum/2;
+        }
+        if(carry!=0){
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+}
