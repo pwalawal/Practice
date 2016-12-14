97c80f7966adec33c071c7b6588d3e92e22518d3
pwalawal <pwalawal@ufl.edu>
2016-12-14 07:05:06 -0500

    Leet code program to find longest common prefix

---
+++ longestPrefix.java
@@ -0,0 +1,17 @@
+//Longest Common Prefix
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0|| strs==null){
+            return "";
+        }
+        
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
