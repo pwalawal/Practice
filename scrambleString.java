b13ad919a336a9d86231aa579b289259248928a5
pwalawal <pwalawal@ufl.edu>
2016-12-29 19:11:43 -0500

    Leet code program to find scramble string

---
+++ scrambleString.java
@@ -0,0 +1,37 @@
+//scramble string
+
+public class Solution {
+    public boolean isScramble(String s1, String s2) {
+        if(s1==null||s2==null|| s1.length()!=s2.length()){
+            return false;
+        }
+        
+        if(s1.equals(s2)){
+            return true;
+        }
+        int len=s1.length();
+        int letters[]=new int[26];
+        for(int i=0;i<s1.length();i++){
+            letters[s1.charAt(i)-'a']++;
+            letters[s2.charAt(i)-'a']--;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(letters[i]!=0){
+                return false;
+            }
+        }
+        
+        for(int i=1;i<len;i++){
+            if(isScramble(s1.substring(0,i),s2.substring(0,i))&&isScramble(s1.substring(i),s2.substring(i)))
+            {
+                return true;
+            }
+            if(isScramble(s1.substring(0,i),s2.substring(len-i))&&isScramble(s1.substring(i),s2.substring(0,len-i))){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
