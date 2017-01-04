b4760e5457531856cbfcc696337d99b956102f09
pwalawal <pwalawal@ufl.edu>
2017-01-03 20:53:44 -0500

    Leet code program to find sumber of segment in string

---
+++ numString.java
@@ -0,0 +1,13 @@
+//Number of Segments in a String
+
+public class Solution {
+    public int countSegments(String s) {
+        
+        s=s.trim();
+        if(s.length()==0){
+            return 0;
+        }
+        return s.split("\\s+").length;
+        
+    }
+}
