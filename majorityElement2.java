beac1c381c379779cdc18102bafb078f3ae6a081
pwalawal <pwalawal@ufl.edu>
2017-02-08 18:50:29 -0500

    Leet code program to find majority element

---
+++ majorityElement2.java
@@ -0,0 +1,22 @@
+Majority Element II
+
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result=new ArrayList<>();
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        
+        for(Integer key:map.keySet()){
+            if(map.get(key)>(nums.length/3)){
+                result.add(key);
+            }
+        }
+        return result;
+    }
+}
