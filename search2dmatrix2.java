86f88ffa61964cd955a0709e255460d7c47d6d40
pwalawal <pwalawal@ufl.edu>
2017-03-20 17:50:13 -0400

    Leet code program to search a number in 2 d matrix

---
+++ search2dmatrix2.java
@@ -0,0 +1,26 @@
+ //Search a 2D Matrix II
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix==null|| matrix.length<1 || matrix[0].length<1){
+            return false;
+        }
+        
+        int row=0;
+        int col=matrix[0].length-1;
+        
+        while(row<matrix.length && col>=0){
+            if(target==matrix[row][col]){
+                return true;
+            }
+            else if(target<matrix[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
