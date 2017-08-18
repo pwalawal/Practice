ad38db9ee55f45357d5c8ebd57bd943dcc878def
pwalawal <pwalawal@ufl.edu>
2017-08-18 14:16:50 -0700

    Leet code program to find max avg subarray

---
+++ maxAvgSubarray.java
@@ -0,0 +1,18 @@
+//Maximum Average Subarray I
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum=0;
+        
+        for(int i=0;i<k;i++){
+            sum=sum+nums[i];
+        }
+        int max=sum;
+        for(int i=k;i<nums.length;i++){
+            sum=sum-nums[i-k]+nums[i];
+            max=Math.max(sum,max);
+        }
+        
+        return ((double)max/(double)k);
+    }
+}
